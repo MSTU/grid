1ae0e252987ec3d4eb33bdfbcacdb23ee5475435
Anton <tf-91@mail.ru>
2013-11-01 16:51:41 +0400

    added setSimulateParameter function to specify simulate parameters listed in mos file

--- DINAMA/plugin/python/grid/solvers/ModelicaSolver.py
+++ DINAMA/plugin/python/grid/solvers/ModelicaSolver.py
@@ -26,207 +26,228 @@ import solvers.Launcher as Launcher
 
 
 class ModelicaSolver(Launcher.Launcher):
-	# инициализация объекта
-	# object initialization
-	def __init__(self):
-		self.name = "ModelicaDynamic"
-		self.MOS_filename = 'script.mos' # Host will use this name to create its own MOS file
+    # инициализация объекта
+    # object initialization
+    def __init__(self):
+        self.name = "ModelicaDynamic"
+        self.MOS_filename = 'script.mos' # Host will use this name to create its own MOS file
 
-	# подговка данных к расчету
-	# preparing of data for calculation
-	# writes dictionary in Loadcase variable inData
-	# keys are mos and mo filenames; values are lists of the file strings
-	def LoadData(self, lc):
-		files_dict = dict() #files dictionary, where keys are filenames and values are list of file strings
-		with open(lc.Scheme, 'r') as f:
-			mos = f.readlines()
-		files_dict[self.MOS_filename] = mos
-		files_dict.update(self.CreateMOfilesDict(lc.Scheme))
-		lc.inData = files_dict
+    # подговка данных к расчету
+    # preparing of data for calculation
+    # writes dictionary in Loadcase variable inData
+    # keys are mos and mo filenames; values are lists of the file strings
+    def LoadData(self, lc):
+        files_dict = dict() #files dictionary, where keys are filenames and values are list of file strings
+        with open(lc.Scheme, 'r') as f:
+            mos = f.readlines()
+        files_dict[self.MOS_filename] = mos
+        files_dict.update(self.CreateMOfilesDict(lc.Scheme))
+        lc.inData = files_dict
 
-	# Создание файлов *.mo и *.mos в папке с именем расчетного случая Loadcase.Name.
-	# Получение exe-файла с их помощью
-	def Compile(self, MOS_filename):
-		if sys.platform.startswith('win'):
-			command = '%OPENMODELICAHOME%/bin/omc.exe ' + MOS_filename
-			print 'compiling...'
-			subprocess.call(["cmd", "/C", command])#, startupinfo=startupinfo)
-			print 'compiled'
-		elif sys.platform.startswith('linux'):
-			print 'compiling...'
-			subprocess.call(["omc", MOS_filename])
-			print 'compiled'
-		else:
-			print 'can not determine your platform'
-			return Constants.TASK_ERROR
+    # Создание файлов *.mo и *.mos в папке с именем расчетного случая Loadcase.Name.
+    # Получение exe-файла с их помощью
+    def Compile(self, MOS_filename):
+        if sys.platform.startswith('win'):
+            command = '%OPENMODELICAHOME%/bin/omc.exe ' + MOS_filename
+            print 'compiling...'
+            subprocess.call(["cmd", "/C", command])#, startupinfo=startupinfo)
+            print 'compiled'
+        elif sys.platform.startswith('linux'):
+            print 'compiling...'
+            subprocess.call(["omc", MOS_filename])
+            print 'compiled'
+        else:
+            print 'can not determine your platform'
+            return Constants.TASK_ERROR
 
-	# запуск расчета схемы и инициализация ее параметрами
-	# scheme - путь к файлу задачи (mos, mo, py, sch)
-	# словарь параметров и их значений
-	def Run(self, loadcase, parameters):
-		cwd = os.getcwd()
-		#if not os.path.exists(id): os.makedirs(id)
-		#os.chdir(id)
-		if not os.path.exists(loadcase.Name): os.makedirs(loadcase.Name)
-		os.chdir(loadcase.Name)
+    # запуск расчета схемы и инициализация ее параметрами
+    # scheme - путь к файлу задачи (mos, mo, py, sch)
+    # словарь параметров и их значений
+    def Run(self, loadcase, parameters):
+        cwd = os.getcwd()
+        #if not os.path.exists(id): os.makedirs(id)
+        #os.chdir(id)
+        if not os.path.exists(loadcase.Name): os.makedirs(loadcase.Name)
+        os.chdir(loadcase.Name)
 
-		# Host creates all required files
-		for k, v in loadcase.inData.iteritems():
-			self.CreateFileFromList(v, k)
+        # Host creates all required files
+        for k, v in loadcase.inData.iteritems():
+            self.CreateFileFromList(v, k)
 
-		class_name = self.GetClassName(self.MOS_filename)
-		''' something for checking file's checksum
-		if sys.platform.startswith('win'):
-			# Проверка, существует ли уже exe-файл модели или нет
-			if (os.path.isfile(class_name + '.exe')):
-				pass
-			else:
-				self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
-				hash = self.hashFile(class_name + '.exe', hashlib.sha256())
-				with open(class_name + '.txt', 'w') as h:
-					h.write(hash)
-		elif sys.platform.startswith('linux'):
-			if (os.path.isfile(class_name)):
-				pass
-			else:
-				self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
-				hash = self.hashFile(class_name + '.exe', hashlib.sha256())
-				with open(class_name + '.txt', 'w') as h:
-					h.write(hash)
-		else:
-			print 'can not determine your platform'
-			return Constants.TASK_ERROR
-		'''
+        class_name = self.GetClassName(self.MOS_filename)
+        ''' something for checking file's checksum
+        if sys.platform.startswith('win'):
+            # Проверка, существует ли уже exe-файл модели или нет
+            if (os.path.isfile(class_name + '.exe')):
+                pass
+            else:
+                self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
+                hash = self.hashFile(class_name + '.exe', hashlib.sha256())
+                with open(class_name + '.txt', 'w') as h:
+                    h.write(hash)
+        elif sys.platform.startswith('linux'):
+            if (os.path.isfile(class_name)):
+                pass
+            else:
+                self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
+                hash = self.hashFile(class_name + '.exe', hashlib.sha256())
+                with open(class_name + '.txt', 'w') as h:
+                    h.write(hash)
+        else:
+            print 'can not determine your platform'
+            return Constants.TASK_ERROR
+        '''
 
-		PAR_filename = 'pl.txt'
-		RES_filename = 'results.plt'
+        PAR_filename = 'pl.txt'
+        RES_filename = 'results.plt'
 
-		# создание файлов входных параметров по словарям входных параметров
-		self.CreateParFilesFromParDicts(PAR_filename, parameters.GetParameters())
+        # создание файлов входных параметров по словарям входных параметров
+        self.CreateParFilesFromParDicts(PAR_filename, parameters.GetParameters())
 
-		if sys.platform.startswith('win'):
-			if (os.path.isfile(class_name + '.exe')):
-				pass
-			else:
-				self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
-			command = class_name + '.exe -overrideFile ' + PAR_filename + ' -r ' + RES_filename
-			print 'executing'
-			subprocess.call(["cmd", "/C", command])#, startupinfo=startupinfo)
-			print 'executed'
+        if sys.platform.startswith('win'):
+            if (os.path.isfile(class_name + '.exe')):
+                pass
+            else:
+                self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
+            command = class_name + '.exe -overrideFile ' + PAR_filename + ' -r ' + RES_filename
+            print 'executing'
+            subprocess.call(["cmd", "/C", command])#, startupinfo=startupinfo)
+            print 'executed'
 
-		elif sys.platform.startswith('linux'):
-			if (os.path.isfile(class_name)):
-				pass
-			else:
-				self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
-			command = ['-overrideFile'] + [PAR_filename] + ['-r'] + [RES_filename]
-			print 'executing'
-			subprocess.call(["./" + class_name] + command)
-			print 'executed'
+        elif sys.platform.startswith('linux'):
+            if (os.path.isfile(class_name)):
+                pass
+            else:
+                self.Compile(self.MOS_filename) # получение exe-файла по mos-файлу
+            command = ['-overrideFile'] + [PAR_filename] + ['-r'] + [RES_filename]
+            print 'executing'
+            subprocess.call(["./" + class_name] + command)
+            print 'executed'
 
-		else:
-			print 'can not determine your platform'
-			return Constants.TASK_ERROR
+        else:
+            print 'can not determine your platform'
+            return Constants.TASK_ERROR
 
-		# получение словаря выходных параметров
-		self.CreateResultsDict(RES_filename, parameters)
-		print parameters.GetResults()
+        # получение словаря выходных параметров
+        self.CreateResultsDict(RES_filename, parameters)
+        print parameters.GetResults()
 
-		os.chdir(cwd)
-		return Constants.TASK_SUCCESS
+        os.chdir(cwd)
+        return Constants.TASK_SUCCESS
+	# using this function you can specify simulate parameters listed in MOS file
+	# for instance: setSimulateParameters('mos_file.mos', 'stopTime', 35.5)
+    def setSimulateParameter(self, MOS_filename, par_name, par_value):
+        temp_file = open('temp', 'w')
+        with open(MOS_filename, 'r') as f:
+            for line in f:
+                if(not line.startswith('simulate')):
+                    temp_file.write(line)
+                else:
+                    temp_file.write(re.sub(par_name + '( )?=( )?(\d)+(\.(\d)+)*', par_name + ' = ' + par_value, line))
+                    #par_name + '( )?=( )?(\d)+(\.(\d)+)*
+                    #line matches above pattern if it contains something like:
+                    #par_name(1 or 0 spaces)=(1 or 0 spaces)(any number of digits)
+                    #par_name(1 or 0 spaces)=(1 or 0 spaces)(any number of digits).(1 digit or more)
+        temp_file.close()
+        #now temp file is a full copy of MOS file with one difference in parameter value
+        #parameter named "par_name" has changed its old value to "par_value"
+        os.unlink(MOS_filename) #deleting old MOS file
+        os.rename(temp_file.name, MOS_filename)
+        #changed temp filename to MOS filename, so we can use old filename with new content
 
-	# Возвращает имя класса, прочитанное из mos-файла (simulate(className, ...))
-	# Необходимо для получения кода модели на языке C, его компиляции и запуска
-	def GetClassName(self, MOS_filename):
-		className = None
-		with open(MOS_filename, 'r') as f:
-			for line in f:
-				if (not line.startswith('simulate')):
-					pass
-				else:
-					className = re.split('[(,]', line)[1]
+    # Возвращает имя класса, прочитанное из mos-файла (simulate(className, ...))
+    # Необходимо для получения кода модели на языке C, его компиляции и запуска
+    def GetClassName(self, MOS_filename):
+        className = None
+        with open(MOS_filename, 'r') as f:
+            for line in f:
+                if (not line.startswith('simulate')):
+                    pass
+                else:
+                    className = re.split('[(,]', line)[1]
 
-		return className
+        return className
 
-	# Returns file's checksum
-	# filename - name of file; hasher - hash algorithm (md5, sha1, sha256, ...);
-	# blocksize - the internal block size of the hash algorithm in bytes
-	def hashFile(self, filename, hasher, blocksize = 8192):
-		with open(filename, 'rb') as f:
-			buf = f.read(blocksize)
-			while len(buf) > 0:
-				hasher.update(buf)
-				buf = f.read(blocksize)
+    # Returns file's checksum
+    # filename - name of file; hasher - hash algorithm (md5, sha1, sha256, ...);
+    # blocksize - the internal block size of the hash algorithm in bytes
+    def hashFile(self, filename, hasher, blocksize = 8192):
+        with open(filename, 'rb') as f:
+            buf = f.read(blocksize)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = f.read(blocksize)
 
-		return hasher.hexdigest()
+        return hasher.hexdigest()
 
-	# Returns a dictionary, where keys are mo filenames and values are list of mo file strings
-	# dictionary is created using mos file located at MOS_file_path
-	def CreateMOfilesDict(self, MOS_file_path):
-		files_dict = dict()
-		cwd = os.getcwd()
-		#filenames.append(re.search('(\w)+\.mos', MOS_file_path).group())
-		with open(MOS_file_path, 'r') as mos:#mos = open(MOS_file_path, 'r')
-			os.chdir(MOS_file_path.rpartition('/')[0])
-			for line in mos:
-				if (not line.startswith('loadFile')):
-					pass
-				else:
-					mo_filename = re.search('(\w)+\.mo', line).group()
-					#mo_filepath = MOS_file_path.rpartition('/')[0] + '/' + mo_filename
-					with open(re.split('"', line)[1], 'r') as mo:
-						mo_file = mo.readlines()
-						files_dict[mo_filename] = mo_file
-						#filenames.append(re.search('(\w)+\.mo', line).group())
-		os.chdir(cwd)
+    # Returns a dictionary, where keys are mo filenames and values are list of mo file strings
+    # dictionary is created using mos file located at MOS_file_path
+    def CreateMOfilesDict(self, MOS_file_path):
+        files_dict = dict()
+        cwd = os.getcwd()
+        #filenames.append(re.search('(\w)+\.mos', MOS_file_path).group())
+        with open(MOS_file_path, 'r') as mos:#mos = open(MOS_file_path, 'r')
+            os.chdir(MOS_file_path.rpartition('/')[0])
+            for line in mos:
+                if (not line.startswith('loadFile')):
+                    pass
+                else:
+                    mo_filename = re.search('(\w)+\.mo', line).group()
+                    #mo_filepath = MOS_file_path.rpartition('/')[0] + '/' + mo_filename
+                    with open(re.split('"', line)[1], 'r') as mo:
+                        mo_file = mo.readlines()
+                        files_dict[mo_filename] = mo_file
+                        #filenames.append(re.search('(\w)+\.mo', line).group())
+        os.chdir(cwd)
 
-		return files_dict
+        return files_dict
 
-	# Создает файл с названием filename по списку строк stringList
-	# Пример содержания переменной stringList: ['loadModel(Modelica);\n', 'getErrorString();\n', ...]
-	def CreateFileFromList(self, stringList, filename):
-		# Создание файла
-		with open(filename, 'w') as f:
-			for line in stringList:
-				f.write(line)
+    # Создает файл с названием filename по списку строк stringList
+    # Пример содержания переменной stringList: ['loadModel(Modelica);\n', 'getErrorString();\n', ...]
+    def CreateFileFromList(self, stringList, filename):
+        # Создание файла
+        with open(filename, 'w') as f:
+            for line in stringList:
+                f.write(line)
 
-	# Генерация файлов входных параметров по словарям входных параметров ma1, ma2, ...
-	def CreateParFilesFromParDicts(self, par_filename, par_dic):
-		with open(par_filename, 'w') as PAR_file:
-			for k, v in par_dic.iteritems():
-				PAR_file.write(k + '=' + str(v) + '\n')
+    # Генерация файлов входных параметров по словарям входных параметров ma1, ma2, ...
+    def CreateParFilesFromParDicts(self, par_filename, par_dic):
+        with open(par_filename, 'w') as PAR_file:
+            for k, v in par_dic.iteritems():
+                PAR_file.write(k + '=' + str(v) + '\n')
 
-	# Создание словаря результатов по файлу результатов RES_filename
-	# для входных параметров parameters
-	# RES_filename - имя файла результатов
-	def CreateResultsDict(self, RES_filename, parameters):
-		layers = 0 # количество временных слоев
-		curvesNumber = 0 #количество выходных переменных
-		result_dict = dict() #словарь результатов
-		value_list = list() #список значений для текущего выходного параметра
-		print os.getcwd()
+    # Создание словаря результатов по файлу результатов RES_filename
+    # для входных параметров parameters
+    # RES_filename - имя файла результатов
+    def CreateResultsDict(self, RES_filename, MA_object):
+        layers = 0 # количество временных слоев
+        curvesNumber = 0 #количество выходных переменных
+        result_dict = dict() #словарь результатов
+        value_list = list() #список значений для текущего выходного параметра
+        print os.getcwd()
 
-		with open(RES_filename, 'r') as f:
-			for line in f:
-				if (not line.startswith('DataSet: ')):
-					continue
-				curvesNumber += 1
-				key = re.split('DataSet: ', line)[1][:-1]
-				for line in f:
-					if (line != '\n'):
-						layers += 1
-						parameters.SetLayer(layers)
-						value_list.append(float(re.split('[\d.e-], ', line)[1][:-1]))
-					else:
-						break
-				tmp = list(value_list[1:-1]) #убрали первый и последний элементы,
-				#т.к. value_list[первый] = value_list[первый+1],
-				#а value_list[последний] = value_list[последний-1]
-				result_dict[key] = tmp
-				del value_list[:] #очистить содержимое всего списка
+        with open(RES_filename, 'r') as f:
+            for line in f:
+                if (not line.startswith('DataSet: ')):
+                    continue
+                curvesNumber += 1
+                MA_object.SetCurves(curvesNumber)
+                key = re.split('DataSet: ', line)[1][:-1]
+                for line in f:
+                    if (line != '\n'):
+                        layers += 1
+                        MA_object.SetLayer(layers)
+                        value_list.append(float(re.split('[\d.e-], ', line)[1][:-1]))
+                    else:
+                        break
+                tmp = list(value_list[1:-1]) #убрали первый и последний элементы,
+                #т.к. value_list[первый] = value_list[первый+1],
+                #а value_list[последний] = value_list[последний-1]
+                result_dict[key] = tmp
+                del value_list[:] #очистить содержимое всего списка
 
-		parameters.SetResults(result_dict)
-		parameters.SetStatus(Constants.TASK_SUCCESS)
+        MA_object.SetResults(result_dict)
+        MA_object.SetStatus(Constants.TASK_SUCCESS)
 
-	def GetLog(self):
-		pass
+    def GetLog(self):
+        pass
