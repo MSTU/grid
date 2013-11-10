ccbe23d32bc797773fa4f1e427b5a91b7f7c7ec3
Anton <tf-91@mail.ru>
2013-11-10 16:11:56 +0400

    Added two tests for class SensitivityGRID with different types of criterias

--- DINAMA/plugin/python/pradis/multi/tests/SensitivityGRID_test_with_user_functions.py
+++ DINAMA/plugin/python/pradis/multi/tests/SensitivityGRID_test_with_user_functions.py
@@ -16,17 +16,26 @@
 #*                                                                         *
 #***************************************************************************/
 
-import pradis.multi.SensitivityGRID as SG
-import pradis.grid.Loadcase as LC
+import Loadcase
 import pradis.multi.Variable as V
+import pradis.multi.SensitivityGRID as SG
 
-#example of working with SensitivityGRID
+def user_criteria(MA_object):
+    layer = MA_object.GetLayerCount()
+    operand = MA_object.GetValueFromLayerByName('resistor1.v', layer)
+    return operand ** 2
 
 def test_1():
-    lc1 = LC.Loadcase ([],['My_script.mos', '', '', 'ModelicaDynamic'], desc = 'lc1')
+
+    lc1 = Loadcase.Loadcase ([],
+        ['mos/mydcmotor.mos', '', [], '', 'ModelicaDynamic', '%', '%'],
+        desc = 'lc1')
     var1 = V.Variable([], ['resistor1.R', 5.0, 1.0, 10.0], desc = 'R')
     var2 = V.Variable([], ['inductor1.L', 0.4, 0.1, 1.0], desc = 'L')
-    sg = SG.Sensitivity('', ['Forward', [var1, var2], [lc1], ['resistor1.v', 'inductor1.v'], 0.1], desc = 'FSG_test')
+    sg = SG.Sensitivity([],
+        ['Forward', [var1, var2], [lc1], [user_criteria], 0.1],
+        desc = 'SG')
+
     sg.Run()
 
 test_1()
